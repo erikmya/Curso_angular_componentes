0e1297203b9ab36b8bbf894facef6bd3b8cb8ec1
erikmya <erik.maya@librerialeo.com.mx>
2022-10-26 12:08:39 -0500

    Clase 3 inputs

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = 'https://i.pinimg.com/originals/9a/66/11/9a66115b0534c437a93abf02db2ca7a1.jpg';
 }
