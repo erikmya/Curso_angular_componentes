87396ddabfd2660a3f6ee83a0338c3fd3ab65c4f
erikmya <erik.maya@librerialeo.com.mx>
2022-10-27 17:35:36 -0500

    Clase 14 Obteniendo datos de una API

--- my-store/src/app/components/products/products.component.ts
+++ my-store/src/app/components/products/products.component.ts
@@ -4,6 +4,8 @@ import { Product } from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service'
 
+import { ProductsService } from '../../services/products.service'
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -13,41 +15,21 @@ export class ProductsComponent implements OnInit {
 
   myShoppingCart: Product[] = [];
   total = 0;
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.png'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/book.jpg'
-    },
-  ];
+  products: Product[] = [];
 
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private productsService: ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
    }
 
   ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    });
   }
 
   onAddToShoppingCart(product: Product) {
