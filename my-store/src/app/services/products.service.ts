5058c130a1ca1b8c3b59569b8b37f4903440a550
erikmya <erik.maya@librerialeo.com.mx>
2022-12-20 17:51:34 -0600

    Clase 12 🌐 Ambientes

--- my-store/src/app/services/products.service.ts
+++ my-store/src/app/services/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { retry } from 'rxjs/operators';
 import { Product, CreateProductDTO, UpdateProductDTO } from '../models/product.model';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private apiUrl = `${environment.API_URL}https://young-sands-07814.herokuapp.com/api/products`;
 
   constructor(
     private http: HttpClient
