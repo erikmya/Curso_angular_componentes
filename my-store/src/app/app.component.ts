2c14b4edc8dc0d37031493d81948ba90f375d30e
erikmya <erik.maya@librerialeo.com.mx>
2022-12-21 17:55:11 -0600

    Clase 21 Descarga de archivos

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   token = '';
 
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private filesService: FilesService
   ) {
 
   }
@@ -36,5 +38,11 @@ export class AppComponent {
     .subscribe(rta => {
       console.log(rta);
     });
+  }
+
+  downloadPdf() {
+    this.filesService.getFile('my.pdf', 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', 'application/pdf')
+    .subscribe()
+
   }
 }
